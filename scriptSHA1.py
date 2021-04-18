64e9d1e241be89a316f452c5f22ccbc10b6eb472
janevski.stefan <38108655+janevski400@users.noreply.github.com>
2021-04-18 03:01:26 +0200

    Create scriptSHA1.py

---
+++ scriptSHA1.py
@@ -0,0 +1,9 @@
+import subprocess
+output = subprocess.run(['sudo','openssl','dgst','-sha1','poraka.txt'], capture_output=True, text=True)
+sha1_value = output.stdout[18:]
+sha1_value = sha1_value.rstrip('\n')
+userSHA1 = input("Vnesete SHA1 hesh vrednost: ")
+if userSHA1==sha1_value:
+    print("SHA1 vrednostite se isti")
+else:
+    print("SHA1 vrednostite ne se isti")
