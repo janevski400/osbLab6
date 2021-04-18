3ebc3d5e177c351012cb630e3eb93b427d113ef8
janevski.stefan <38108655+janevski400@users.noreply.github.com>
2021-04-18 15:41:14 +0200

    Update scriptSHA256HMAC.py

--- scriptSHA256HMAC.py
+++ scriptSHA256HMAC.py
@@ -1,7 +1,7 @@
 import subprocess
 password = input('Vnesete password: ')
 output = subprocess.run(['sudo', 'openssl', 'dgst' , '-sha256','-hmac', password, 'poraka.txt'], capture_output=True, text=True)
-#so pomosh na shlex.split se dobiva sledniot format za argumenti ['sudo', 'openssl', 'dgst', '-sha256', '-hmac', 'gjuro', 'poraka.txt']
+#so pomosh na shlex.split se dobiva sledniot format za argumenti ['sudo', 'openssl', 'dgst', '-sha256', '-hmac', '!!tuka password!!', 'poraka.txt']
 sha256_value = output.stdout[25:]
 sha256_value = sha256_value.rstrip('\n')
 userPassword = input('Vnesete hash256 vrednost so koja sakate da sporedite: ')
@@ -9,4 +9,3 @@ if sha256_value==userPassword:
     print("Dvata hasha se isti")
 else:
     print("Dvata hasha ne se isti")
-    
