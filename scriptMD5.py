f1b508e4a729d8c5188590410695aec61dda02d4
janevski.stefan <38108655+janevski400@users.noreply.github.com>
2021-04-18 03:03:24 +0200

    Update scriptMD5.py

--- scriptMD5.py
+++ scriptMD5.py
@@ -2,7 +2,7 @@ import subprocess
 output = subprocess.run(['sudo','openssl','dgst','-md5','poraka.txt'], capture_output=True, text=True)
 md5_value = output.stdout[17:]
 md5_value = md5_value.rstrip('\n')
-userMD5 = input("Vnesete MD5 hesh vrednost: ")
+userMD5 = input("Vnesete MD5 hash vrednost: ")
 if userMD5==md5_value:
     print("MD5 vrednostite se isti")
 else:
