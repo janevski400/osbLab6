a4229fe09435286e15f8f8e2b13ddf9c2e378fb2
janevski.stefan <38108655+janevski400@users.noreply.github.com>
2021-04-20 20:56:42 +0200

    Create scriptAES.py
    За правилна работа на скриптата потребно е во ист фолдер со неа да има датотека која содржи листа од лозинки (секоја лозинка во нов ред) и poraka.aes (датотека енкриптирана со aes-128-ecb).

---
+++ scriptAES.py
@@ -0,0 +1,23 @@
+import subprocess
+
+def aesTest(password):
+    subprocess.run(['sudo','openssl','enc','-d','-aes-128-ecb','-in','poraka.aes','-out','output.txt','-nosalt', '-k', password], text=True, capture_output=True)
+    try:
+        tempVar = open('output.txt', encoding='utf-8', mode='r').read()
+        if tempVar:
+            return 1
+        else:
+            return 0
+    except:
+        return 0    
+
+password_file = open('password.txt','r')
+for line in password_file:
+    if aesTest(line.rstrip('\n'))==1:   
+        print("Passwordot e: "+line.rstrip('\n'))
+        exit()       
+        subprocess.run('rm output.txt')
+    else:
+        print(line.rstrip('\n') +" ne e tochniot password.")
+
+print("Ne e pronajden tochen password.")
